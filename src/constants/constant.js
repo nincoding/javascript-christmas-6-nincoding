7f1c8761c946d6cbf944f796e142eff954a58f7c
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-11 06:45:45 +0900

    :sparkles: feat: 메뉴 리스트 상수화 Set으로 만들어서 이터러블 활용 및 중복값 제거

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -12,6 +12,13 @@ const DAY = Object.freeze({
   weekday: '평일',
 });
 
+const MENU = Object.freeze({
+  appetizer: new Set(['양송이수프', '타파스', '시저샐러드']),
+  mainDish: new Set(['티본스테이크', '바비큐립', '해산물파스타', '크리스마스파스타']),
+  dessert: new Set(['초코케이크', '아이스크림']),
+  drink: new Set(['제로콜라', '레드와인', '샴페인']),
+});
+
 const BADGE_PRIZE = Object.freeze({
   star: 5_000,
   tree: 10_000,
@@ -43,4 +50,5 @@ export {
   SALE_PRIZE,
   EVENT_PLANNER,
   DAY,
+  MENU,
 };
