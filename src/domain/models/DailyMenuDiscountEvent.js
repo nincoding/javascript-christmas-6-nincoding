036fb53e4825695f7f54cde2f6f3b36e8ed668f1
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-11 06:46:32 +0900

    :sparkles: feat: 평일과 주말에 각각 디저트, 메인메뉴를 주문개수만큼 할인한 금액을 반환한다.

--- src/domain/models/DailyMenuDiscountEvent.js
+++ src/domain/models/DailyMenuDiscountEvent.js
@@ -1,4 +1,4 @@
-import { EVENT_PLANNER, DAY } from '../../constants/constant.js';
+import { EVENT_PLANNER, DAY, MENU } from '../../constants/constant.js';
 
 class DailyMenuDiscountEvent {
   #visitDay;
@@ -6,7 +6,7 @@ class DailyMenuDiscountEvent {
 
   constructor(visitDate, orderReceipt) {
     this.#visitDay = this.#distinguishVisitDay(visitDate);
-    this.#dailySalePrice = this.#calcMenuDiscount(orderReceipt);
+    this.#dailySalePrice = this.#dailyMenuDiscount(orderReceipt);
   }
 
   #distinguishVisitDay(visitDate) {
@@ -15,7 +15,54 @@ class DailyMenuDiscountEvent {
     return visitWeekEnd ? DAY.weekend : DAY.weekday;
   }
 
-  #calcMenuDiscount(orderReceipt) {}
+  #dailyMenuDiscount(orderReceipt) {
+    return this.#visitDay === DAY.weekend
+      ? this.#calculateWeekendDiscount(orderReceipt)
+      : this.#calculateWeekdayDiscount(orderReceipt);
+  }
+
+  #calculateWeekendDiscount(orderReceipt) {
+    if (this.#isContainMainDish(orderReceipt)) {
+      const totalMainDishCount = this.#calculateTotalItemCount(orderReceipt, MENU.mainDish);
+
+      return totalMainDishCount * 2_023;
+    }
+
+    return 0;
+  }
+
+  #calculateWeekdayDiscount(orderReceipt) {
+    if (this.#isContainDessert(orderReceipt)) {
+      const totalDessertCount = this.#calculateTotalItemCount(orderReceipt, MENU.dessert);
+
+      return totalDessertCount * 2_023;
+    }
+
+    return 0;
+  }
+
+  #isContainMainDish(orderReceipt) {
+    return this.#isContainItem(orderReceipt, [...MENU.mainDish]);
+  }
+
+  #isContainDessert(orderReceipt) {
+    return this.#isContainItem(orderReceipt, [...MENU.dessert]);
+  }
+
+  #isContainItem(orderReceipt, menuCategory) {
+    const menuNames = Object.keys(orderReceipt);
+
+    return menuNames.some((menuName) => menuCategory.includes(menuName));
+  }
+
+  #calculateTotalItemCount(orderReceipt, menuCategory) {
+    const menuNames = Object.keys(orderReceipt);
+
+    return menuNames.reduce(
+      (total, menuName) => (menuCategory.has(menuName) ? total + orderReceipt[menuName] : total),
+      0
+    );
+  }
 
   getVisitDay() {
     return this.#visitDay;
