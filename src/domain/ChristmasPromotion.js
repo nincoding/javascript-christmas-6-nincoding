dfdb2d22fd42dffe6f9c731bd903ba3f56257886
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-11 07:34:42 +0900

    :sparkles: feat: 크리스마스 프로모션 도메인 스켈레톤 추가

---
+++ src/domain/ChristmasPromotion.js
@@ -0,0 +1,26 @@
+class ChristmasPromotion {
+  #totalOrderAmount;
+  #promotionInfo;
+  #totalSaleAmount;
+  #estimatedAmount;
+
+  constructor() {}
+
+  getTotalOrderAmount() {
+    return this.#totalOrderAmount;
+  }
+
+  getPromotionInfo() {
+    return this.#promotionInfo;
+  }
+
+  getTotalSaleAmount() {
+    return this.#totalSaleAmount;
+  }
+
+  getEstimatedAmount() {
+    return this.#estimatedAmount;
+  }
+}
+
+export default ChristmasPromotion;
