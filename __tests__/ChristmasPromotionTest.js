2ef71f69ad75c381e32c8da2f40e1ac151385b4c
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-11 17:14:42 +0900

    :white_check_mark: test:  총 할인된 혜택 금액이 모두 더해져서 반환하는지 테스트함

--- __tests__/ChristmasPromotionTest.js
+++ __tests__/ChristmasPromotionTest.js
@@ -4,11 +4,13 @@ describe('ChristmasPromotion', () => {
   let christmasPromotion;
   let resultTotalOrderAmount;
   let resultTotalSaleInfo;
+  let resultTotalSaleAmount;
 
   const createPromotion = (visitDate, orderReceipt) => {
     christmasPromotion = new ChristmasPromotion(visitDate, orderReceipt);
     resultTotalOrderAmount = () => christmasPromotion.getTotalOrderAmount();
     resultTotalSaleInfo = () => christmasPromotion.getTotalSaleInfo();
+    resultTotalSaleAmount = () => christmasPromotion.calcTotalSaleAmount();
   };
 
   const totalOrderCases = [
@@ -22,12 +24,14 @@ describe('ChristmasPromotion', () => {
       3,
       { 티본스테이크: 1, 바비큐립: 1, 초코케이크: 2, 제로콜라: 1 },
       { christmasDday: 1_200, special: 1_000, champagne: 25_000, 평일: 4_046 },
+      31_246,
     ],
-    [26, { 타파스: 1, 제로콜라: 1 }, { christmasDday: 0, special: 0, champagne: 0, 평일: 0 }],
+    [26, { 타파스: 1, 제로콜라: 1 }, { christmasDday: 0, special: 0, champagne: 0, 평일: 0 }, 0],
     [
       2,
       { 양송이수프: 1, 해산물파스타: 1 },
       { christmasDday: 1_100, special: 0, champagne: 0, 주말: 2_023 },
+      3_123,
     ],
   ];
 
@@ -42,10 +46,19 @@ describe('ChristmasPromotion', () => {
 
   test.each(totalInfoCases)(
     '적용된 이벤트 할인들의 총 혜택 금액이 알맞게 계산되는지 테스트한다.',
-    (visitDate, orderReceipt, expectedInfo) => {
+    (visitDate, orderReceipt, expectedInfo, _) => {
       createPromotion(visitDate, orderReceipt);
 
       expect(resultTotalSaleInfo()).toStrictEqual(expectedInfo);
+    }
+  );
+
+  test.each(totalInfoCases)(
+    '총 할인 금액이 알맞게 더해져 반환되는지 테스트한다.',
+    (visitDate, orderReceipt, _, expectedAmount) => {
+      createPromotion(visitDate, orderReceipt);
+
+      expect(resultTotalSaleAmount()).toBe(expectedAmount);
     }
   );
 });
