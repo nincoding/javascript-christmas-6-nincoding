8792c9c5437e1e4d8c9628caa81d18c06022663d
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-12 15:41:02 +0900

    :recycle: refactor: 방문날짜 검증로직에서 중복되는 부분을 최소화 시킴

--- src/domain/validators/VisitDateValidator.js
+++ src/domain/validators/VisitDateValidator.js
@@ -1,16 +1,11 @@
+import throwValidationError from '../../utils/throwValidationError.js';
+import { ERROR_MESSAGE } from '../../constants/errorMessage.js';
+
 class VisitDateValidator {
   static validate(visitDate) {
-    if (!this.isNumeric(visitDate)) {
-      throw new Error(`[ERROR] 유효하지 않은 날짜입니다.`);
-    }
-
-    if (!this.isValidRange(visitDate)) {
-      throw new Error(`[ERROR] 유효하지 않은 날짜입니다.`);
-    }
-
-    if (!this.isValidInteger(visitDate)) {
-      throw new Error(`[ERROR] 유효하지 않은 날짜입니다.`);
-    }
+    throwValidationError(!this.isNumeric(visitDate), ERROR_MESSAGE.invalidDate);
+    throwValidationError(!this.isValidRange(visitDate), ERROR_MESSAGE.invalidDate);
+    throwValidationError(!this.isValidInteger(visitDate), ERROR_MESSAGE.invalidDate);
   }
 
   static isNumeric(visitDate) {
