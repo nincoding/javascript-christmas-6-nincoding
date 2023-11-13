e7b968c64a78864e4814b99b505f9b3715f42218
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-13 21:53:37 +0900

    :sparkles: feat: 서비스 시작 문구 및 방문날짜 출력,에러메세지, 주문메뉴에 대한 출력기능 추가

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,9 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+import OUTPUT_MESSAGE from '../constants/outputMessage.js';
+
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printErrorMessage(message) {
+    Console.print(message);
   },
-  // ...
+
+  printStartService() {
+    Console.print(OUTPUT_MESSAGE.startService);
+  },
+
+  printPreviewbenefits(visitDate) {
+    Console.print(OUTPUT_MESSAGE.previewbenefits(visitDate));
+  },
+
+  printOrderMenuInfo(orderMenu) {
+    Console.print(OUTPUT_MESSAGE.orderMenu);
+
+    orderMenu.forEach((menu) => {
+      const menuName = Object.keys(menu)[0];
+      const count = menu[menuName];
+
+      Console.print(`${menuName} ${count}개`);
+    });
+  },
 };
 
 export default OutputView;
