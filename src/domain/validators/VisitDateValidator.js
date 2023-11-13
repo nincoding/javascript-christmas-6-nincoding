83efbe1bc57d48093c98793881fc5b4a04302cc6
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-13 21:11:09 +0900

    :recycle: refactor: 숫자 유효성 검증에 대한 로직을 유틸함수로 분리시킴 다른 유효성 검증 클래스에서도 공통으로 사용되므로 재사용성을 위해 유틸함수로 분리

--- src/domain/validators/VisitDateValidator.js
+++ src/domain/validators/VisitDateValidator.js
@@ -1,25 +1,16 @@
 import throwValidationError from '../../utils/throwValidationError.js';
+import { isNumeric, isInteger } from '../../utils/numericValidation.js';
 import { ERROR_MESSAGE } from '../../constants/errorMessage.js';
 
 class VisitDateValidator {
   static validate(visitDate) {
-    throwValidationError(!this.isNumeric(visitDate), ERROR_MESSAGE.invalidDate);
     throwValidationError(!this.isValidRange(visitDate), ERROR_MESSAGE.invalidDate);
-    throwValidationError(!this.isValidInteger(visitDate), ERROR_MESSAGE.invalidDate);
-  }
-
-  static isNumeric(visitDate) {
-    const numberRegExp = /^\d+$/;
-
-    return numberRegExp.test(visitDate) && !Number.isNaN(Number(visitDate));
+    throwValidationError(!isNumeric(visitDate), ERROR_MESSAGE.invalidDate);
+    throwValidationError(!isInteger(visitDate), ERROR_MESSAGE.invalidDate);
   }
 
   static isValidRange(visitDate) {
     return visitDate >= 1 && visitDate <= 31;
-  }
-
-  static isValidInteger(visitDate) {
-    return Number.isInteger(Number(visitDate));
   }
 }
 
