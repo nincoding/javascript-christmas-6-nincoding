408965ab6d165cd69d30c6debb174031e6787474
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-14 06:29:57 +0900

    :recycle: refactor: 출력뷰 상수화 및 재사용코드 리팩토링

--- src/constants/outputMessage.js
+++ src/constants/outputMessage.js
@@ -3,10 +3,14 @@ const OUTPUT_MESSAGE = Object.freeze({
   previewbenefits: (visitDate) =>
     `12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
   orderMenu: '<주문 메뉴>',
+  formatOrderMenu: (menuName, count) => `${menuName} ${count}개`,
   totalOrderAmount: '<할인 전 총주문 금액>',
   presentedMenu: '<증정 메뉴>',
+  formatPresent: (presentedChampagne) => `샴페인 ${presentedChampagne}개`,
   promotions: '<혜택 내역>',
+  formatSaleInfo: (promotion, amountString) => `${promotion}: -${amountString}원`,
   totalSaleAmount: '<총혜택 금액>',
+  formatSaleAmount: (prefixMessage, amountString) => `${prefixMessage}-${amountString}원`,
   estimatedAmount: '<할인 후 예상 결제 금액>',
   eventBadge: '<12월 이벤트 배지>',
 });
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,11 +1,104 @@
 import { Console } from '@woowacourse/mission-utils';
 import addCommaAmount from '../utils/addCommaAmount.js';
 import OUTPUT_MESSAGE from '../constants/outputMessage.js';
-import { EMPTY_VALUE } from '../constants/constant.js';
+import { EMPTY_VALUE, EMPTY_COUNT } from '../constants/constant.js';
 
 const OutputView = {
   printErrorMessage(message) {
     Console.print(message);
+  },
+
+  printStartService() {
+    Console.print(OUTPUT_MESSAGE.startService);
+  },
+
+  /**
+   * 미리보기 메세지를 방문날짜를 넣어서 출력한다.
+   * @param {number} visitDate
+   */
+  printPreviewbenefits(visitDate) {
+    Console.print(OUTPUT_MESSAGE.previewbenefits(visitDate));
+  },
+
+  /**
+   * 모든 주문메뉴를 메뉴와 개수로 줄바꿔 출력한다.
+   * @param {Array<{ menuName: number }>} orderMenu
+   */
+  printOrderMenuInfo(orderMenu) {
+    Console.print(`\n${OUTPUT_MESSAGE.orderMenu}`);
+
+    orderMenu.forEach((menu) => {
+      const menuName = Object.keys(menu)[0];
+      const count = menu[menuName];
+
+      Console.print(OUTPUT_MESSAGE.formatOrderMenu(menuName, count));
+    });
+  },
+
+  // 할인 전 총주문 금액을 출력한다.
+  printTotalOrderAmount(totalOrderAmount) {
+    const prefixMessage = this.printPrefixMessage(OUTPUT_MESSAGE.totalOrderAmount);
+
+    this.printAmountMessage(prefixMessage, totalOrderAmount);
+  },
+
+  /**
+   * 증정된 샴페인의 개수를 전달받아 출력한다.
+   * @param {number} presentedChampagne
+   */
+  printPresentedMenu(presentedChampagne) {
+    Console.print(`\n${OUTPUT_MESSAGE.presentedMenu}`);
+
+    presentedChampagne > EMPTY_COUNT
+      ? Console.print(OUTPUT_MESSAGE.formatPresent(presentedChampagne))
+      : Console.print(EMPTY_VALUE);
+  },
+
+  /**
+   * 각 혜택 내역과 할인된 금액을 전체적으로 출력한다.
+   * @param {Object} totalSaleInfo - { promotion: amount }
+   */
+  printTotalSaleInfo(totalSaleInfo) {
+    Console.print(`\n${OUTPUT_MESSAGE.promotions}`);
+
+    if (Object.values(totalSaleInfo).every((amount) => amount === EMPTY_COUNT)) {
+      return Console.print(EMPTY_VALUE);
+    }
+
+    Object.entries(totalSaleInfo).forEach(([promotion, amount]) => {
+      const amountString = addCommaAmount(amount);
+
+      if (amount !== EMPTY_COUNT) {
+        Console.print(OUTPUT_MESSAGE.formatSaleInfo(promotion, amountString));
+      }
+    });
+  },
+
+  /**
+   * 총혜택 금액을 출력한다.
+   * @param {number} totalSaleAmount
+   */
+  printTotalSaleAmount(totalSaleAmount) {
+    const prefixMessage = this.printPrefixMessage(OUTPUT_MESSAGE.totalSaleAmount);
+    const amountString = addCommaAmount(totalSaleAmount);
+
+    totalSaleAmount > EMPTY_COUNT
+      ? Console.print(OUTPUT_MESSAGE.formatSaleAmount(prefixMessage, amountString))
+      : this.printAmountMessage(prefixMessage, totalSaleAmount);
+  },
+
+  // 할인 후 예상 결제 금액을 출력한다.
+  printEstimatedAmount(estimatedAmount) {
+    const prefixMessage = this.printPrefixMessage(OUTPUT_MESSAGE.estimatedAmount);
+
+    this.printAmountMessage(prefixMessage, estimatedAmount);
+  },
+
+  // 12월 이벤트 배지를 출력한다.
+  printBadge(badge) {
+    const prefixMessage = this.printPrefixMessage(OUTPUT_MESSAGE.eventBadge);
+
+    Console.print(`${prefixMessage}${badge}`);
   },
 
   printAmountMessage(prefixMessage, amount) {
@@ -14,82 +107,8 @@ const OutputView = {
     return Console.print(`${prefixMessage}${result}원`);
   },
 
-  // 서비스 시작 문구를 출력한다.
-  printStartService() {
-    Console.print(OUTPUT_MESSAGE.startService);
-  },
-
-  // 미리보기 메세지를 출력한다.
-  printPreviewbenefits(visitDate) {
-    Console.print(OUTPUT_MESSAGE.previewbenefits(visitDate));
-  },
-
-  // 주문 메뉴를 출력한다.
-  printOrderMenuInfo(orderMenu) {
-    Console.print(OUTPUT_MESSAGE.orderMenu);
-
-    orderMenu.forEach((menu) => {
-      const menuName = Object.keys(menu)[0];
-      const count = menu[menuName];
-
-      Console.print(`${menuName} ${count}개`);
-    });
-  },
-
-  // 할인 전 총주문 금액을 출력한다.
-  printTotalOrderAmount(totalOrderAmount) {
-    const prefixMessage = `${OUTPUT_MESSAGE.totalOrderAmount}\n`;
-
-    this.printAmountMessage(prefixMessage, totalOrderAmount);
-  },
-
-  // 증정 메뉴를 출력한다.
-  printPresentedMenu(presentedChampagne) {
-    Console.print(OUTPUT_MESSAGE.presentedMenu);
-
-    if (presentedChampagne > 0) {
-      return Console.print(`샴페인 ${presentedChampagne}개`);
-    }
-
-    Console.print(EMPTY_VALUE);
-  },
-
-  // 혜택 내역을 출력한다.
-  printTotalSaleInfo(totalSaleInfo) {
-    Console.print(OUTPUT_MESSAGE.promotions);
-
-    if (Object.values(totalSaleInfo).every((amount) => amount === 0)) {
-      return Console.print(EMPTY_VALUE);
-    }
-
-    return Object.entries(totalSaleInfo).forEach(([promotion, amount]) => {
-      if (amount !== 0) Console.print(`${promotion}: -${addCommaAmount(amount)}원`);
-    });
-  },
-
-  // 총혜택 금액을 출력한다.
-  printTotalSaleAmount(totalSaleAmount) {
-    const prefixMessage = `${OUTPUT_MESSAGE.totalSaleAmount}\n`;
-
-    if (totalSaleAmount > 0) {
-      return Console.print(`${prefixMessage}-${addCommaAmount(totalSaleAmount)}원`);
-    }
-
-    return this.printAmountMessage(prefixMessage, totalSaleAmount);
-  },
-
-  // 할인 후 예상 결제 금액을 출력한다.
-  printEstimatedAmount(estimatedAmount) {
-    const prefixMessage = `${OUTPUT_MESSAGE.estimatedAmount}\n`;
-
-    this.printAmountMessage(prefixMessage, estimatedAmount);
-  },
-
-  // 12월 이벤트 배지를 출력한다.
-  printBadge(badge) {
-    const prefixMessage = `${OUTPUT_MESSAGE.eventBadge}\n`;
-
-    Console.print(`${prefixMessage}${badge}`);
+  printPrefixMessage(prefixMessage) {
+    return `\n${prefixMessage}\n`;
   },
 };
 
