7ecb47eb248cba9c5c49ee3ca98cd9aeab5f5bce
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-13 22:33:48 +0900

    :sparkles: feat: 금액 출력부분과 배지 출력기능 추가 해당 출력값을 설명하는 메세지문구와 함께 데이터값을 출력시킨다. 총혜택금액이 0이면 앞에 -를 생략한다. 모든 금액은 세자리마다 콤마를 붙여 출력한다.

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import addCommaAmount from '../utils/addCommaAmount.js';
 import OUTPUT_MESSAGE from '../constants/outputMessage.js';
 
 const OutputView = {
@@ -6,14 +7,21 @@ const OutputView = {
     Console.print(message);
   },
 
+  printStaticMessage(prefixMessage, message) {
+    return Console.print(`${prefixMessage}${message}`);
+  },
+
+  // 서비스 시작 문구를 출력한다.
   printStartService() {
     Console.print(OUTPUT_MESSAGE.startService);
   },
 
+  // 미리보기 메세지를 출력한다.
   printPreviewbenefits(visitDate) {
     Console.print(OUTPUT_MESSAGE.previewbenefits(visitDate));
   },
 
+  // 주문 메뉴를 출력한다.
   printOrderMenuInfo(orderMenu) {
     Console.print(OUTPUT_MESSAGE.orderMenu);
 
@@ -24,6 +32,43 @@ const OutputView = {
       Console.print(`${menuName} ${count}개`);
     });
   },
+
+  // 할인 전 총주문 금액을 출력한다.
+  printTotalOrderAmount(totalOrderAmount) {
+    const prefixMessage = `${OUTPUT_MESSAGE.totalOrderAmount}\n`;
+    const message = addCommaAmount(totalOrderAmount);
+
+    this.printStaticMessage(prefixMessage, message);
+  },
+
+  // 총혜택 금액을 출력한다.
+  printTotalSaleAmount(totalSaleAmount) {
+    const prefixMessage = `${OUTPUT_MESSAGE.totalSaleAmount}\n`;
+    const message = addCommaAmount(totalSaleAmount);
+
+    if (totalSaleAmount > 0) return Console.print(`${prefixMessage}-${message}원`);
+
+    return Console.print(`${prefixMessage}${message}원`);
+  },
+
+  // 할인 후 예상 결제 금액을 출력한다.
+  printEstimatedAmount(estimatedAmount) {
+    const prefixMessage = `${OUTPUT_MESSAGE.estimatedAmount}\n`;
+    const message = addCommaAmount(estimatedAmount);
+
+    this.printStaticMessage(prefixMessage, message);
+  },
+
+  // 이벤트 배지를 출력한다.
+  printBadge(badge) {
+    const prefixMessage = `${OUTPUT_MESSAGE.eventBadge}\n`;
+
+    this.printStaticMessage(prefixMessage, badge);
+  },
+
+  // todo: 증정 메뉴를 출력한다.
+
+  // todo: 혜택 내역을 출력한다.
 };
 
 export default OutputView;
